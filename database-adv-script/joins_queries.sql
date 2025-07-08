6719a7455bd34a68c06b21322380928f91b47e68
Nyakio-Eva <evalkariuki@gmail.com>
2025-07-08 21:16:34 +0300

    correction for left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,33 +1,20 @@
 -- INNER JOIN – Bookings with User Info
 SELECT 
     bookings.id AS booking_id,
-    bookings.start_date,
-    bookings.end_date,
-    users.id AS user_id,
-    users.email
+    users.id AS user_id
 FROM 
     bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
 --  LEFT JOIN – Properties and Their Reviews
-SELECT 
-    properties.id AS property_id,
-    properties.title,
-    reviews.id AS review_id,
-    reviews.rating,
-    reviews.comment
+SELECT *
 FROM 
     properties
 LEFT JOIN reviews ON properties.id = reviews.property_id;
 
 -- FULL OUTER JOIN – Users and Bookings (All Rows)
 
-SELECT 
-    users.id AS user_id,
-    users.email,
-    bookings.id AS booking_id,
-    bookings.start_date,
-    bookings.end_date
+SELECT *
 FROM 
     users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+FULL OUTER JOIN bookings ON users.users.id = bookings.user_id;
